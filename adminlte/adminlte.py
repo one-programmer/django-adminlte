fa118b2c5b7c133686f4884a359735949565f3bf
ZhangBohan <bohanzhang@foxmail.com>
2016-12-22 15:48:04 +0800

    增加群组后台页面
    Fix #6

--- adminlte/adminlte.py
+++ adminlte/adminlte.py
@@ -6,7 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render, redirect
 from django.db.models import Q
 
-from .forms import PermissionForm
+from .forms import PermissionForm, GroupForm
 from .utils import AdminLTEBaseView, AdminMenu, Pager
 
 
@@ -68,7 +68,7 @@ class PermissionsView(AdminLTEBaseView):
 
 class PermissionsCreateView(AdminLTEBaseView):
 
-    menu = AdminMenu('Add Permissions', parent_menu=permission_group_menu)
+    menu = AdminMenu('Add Permission', parent_menu=permission_group_menu)
 
     template_name = 'adminlte/permissions/edit.html'
 
@@ -118,6 +118,91 @@ class PermissionEditView(AdminLTEBaseView):
             permission.save()
             messages.add_message(request, messages.SUCCESS, 'edit success')
             return redirect('adminlte.permissions')
+
+        messages.add_message(request, messages.ERROR, 'params error')
+        return render(request, self.template_name)
+
+
+class GroupsView(AdminLTEBaseView):
+
+    menu = AdminMenu('Groups', parent_menu=permission_group_menu)
+
+    def get(self, request, *args, **kwargs):
+        search = request.GET.get('search', '')
+
+        query = Group.objects.all()
+
+        if search:
+            query = query.filter(name__contains=search)
+
+        pager = Pager.from_request(query, request)
+        return render(request, 'adminlte/groups/index.html', context={
+            "pager": pager,
+            "search": search
+        })
+
+
+class GroupCreateView(AdminLTEBaseView):
+
+    menu = AdminMenu('Add Group', parent_menu=permission_group_menu)
+
+    template_name = 'adminlte/groups/edit.html'
+
+    def post(self, request, *args, **kwargs):
+        form = GroupForm(request.POST)
+
+        if form.is_valid():
+            Group.objects.create(name=form.cleaned_data['name'])
+            messages.add_message(request, messages.SUCCESS, 'add success')
+            return redirect('adminlte.groups')
+
+        messages.add_message(request, messages.ERROR, 'params error')
+        return render(request, self.template_name)
+
+
+class GroupDeleteView(AdminLTEBaseView):
+
+    _regex_name = 'groups/(?P<pk>[0-9]+)/delete'
+
+    def get(self, request, pk, *args, **kwargs):
+        group = Group.objects.get(pk=pk)
+        group.delete()
+        messages.add_message(request, messages.SUCCESS, 'delete success')
+        return redirect('adminlte.groups')
+
+
+class GroupEditView(AdminLTEBaseView):
+    _regex_name = 'groups/(?P<pk>[0-9]+)/edit'
+
+    template_name = 'adminlte/groups/edit.html'
+
+    def get(self, request, pk, *args, **kwargs):
+        group = Group.objects.get(pk=pk)
+        user_permissions = group.permissions.all()
+        permissions = Permission.objects.all()
+        return render(request, self.template_name, context={
+            "group": group,
+            "permissions": permissions,
+            "user_permissions": user_permissions,
+        })
+
+    def post(self, request, pk, *args, **kwargs):
+        form = GroupForm(request.POST)
+
+        if form.is_valid():
+            group = Group.objects.get(pk=pk)
+            group.name = form.cleaned_data['name']
+            permissions = form.cleaned_data['permissions']
+            delete_permissions = set(group.permissions.all()) - set(permissions)
+            add_permissions = set(permissions) - set(group.permissions.all())
+            for permission in delete_permissions:
+                group.permissions.remove(permission)
+
+            for permission in add_permissions:
+                group.permissions.add(permission)
+            group.save()
+            messages.add_message(request, messages.SUCCESS, 'edit success')
+            return redirect('adminlte.group.edit', pk=group.id)
 
         messages.add_message(request, messages.ERROR, 'params error')
         return render(request, self.template_name)
--- adminlte/forms.py
+++ adminlte/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.contrib.auth.models import Permission
 
 
 class PermissionForm(forms.Form):
     codename = forms.CharField()
     name = forms.CharField()
+
+
+class GroupForm(forms.Form):
+    name = forms.CharField()
+    permissions = forms.ModelMultipleChoiceField(queryset=Permission.objects.all(), required=False)
